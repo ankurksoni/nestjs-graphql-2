21f0f8d6ae4ca32d23d36413fd3aac03f95337b3
ankurksoni <ankursoni.leonardo@gmail.com>
2023-07-08 09:54:38 +0530

    chores: Add dummy data.

--- src/contacts/services/contacts.service.ts
+++ src/contacts/services/contacts.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { Contact } from "../entities/contacts.entity";
 
 @Injectable()
 export class ContactsService {
+
+    private fakeContacts: Contact[] = [
+        {
+            phoneNumber: "123123213",
+            id: 1,
+            isPublic: true
+        }
+    ]
+
     getPublicContacts() {
-        
+        return this.fakeContacts;
     }
 }
